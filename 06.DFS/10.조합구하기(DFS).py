7ecb810ae48080854a4f6c719807ffdd41c820ae
liaminjeong <d.minjeong2@gmail.com>
2022-11-05 09:56:46 +0900

    조합구하기

---
+++ 06.DFS/10.조합구하기(DFS).py
@@ -0,0 +1,19 @@
+import sys
+def DFS(L,S):
+    global cnt
+    if L ==m:
+        for j in range(m):
+            print(res[j],end= ' ')
+        cnt+=1
+        print()
+    else:
+        for i in range(S,n+1):
+            res[L] = i
+            DFS(L+1,i+1)
+
+if __name__=="__main__":
+    n,m = map(int,input().split())
+    res =[0]*m
+    cnt=0
+    DFS(0,1)
+    print(cnt)
